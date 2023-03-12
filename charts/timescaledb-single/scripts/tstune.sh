c6ebfe21fc4c7c4958691c35302b21c7246a8015
joschiwald <736792+joschiwald@users.noreply.github.com>
2023-03-12 14:54:18 +0100

    charts/timescaledb-single: pass wal-disk-size to timescaledb-tune to fix wrong wal sizes are set after tune

--- charts/timescaledb-single/scripts/tstune.sh
+++ charts/timescaledb-single/scripts/tstune.sh
@@ -30,26 +30,7 @@ if [ -f "${PGDATA}/postgresql.base.conf" ] && ! grep "include_if_exists = '${TST
     echo "include_if_exists = '${TSTUNE_FILE}'" >> "${PGDATA}/postgresql.base.conf"
 fi
 
-# If there is a dedicated WAL Volume, we want to set max_wal_size to 60% of that volume
-# If there isn't a dedicated WAL Volume, we set it to 20% of the data volume
-if [ "${WAL_VOLUME_SIZE}" = "0" ]; then
-    WALMAX="${DATA_VOLUME_SIZE}"
-    WALPERCENT=20
-else
-    WALMAX="${WAL_VOLUME_SIZE}"
-    WALPERCENT=60
-fi
-
-WALMAX=$(numfmt --from=auto "${WALMAX}")
-
-# Wal segments are 16MB in size, in this way we get a "nice" number of the nearest
-# 16MB
-# walmax / 100 * walpercent / 16MB # below is a refactored with increased precision
-WALMAX=$(( WALMAX * WALPERCENT * 16 / 16777216 / 100  ))
-WALMIN=$(( WALMAX / 2 ))
-
-echo "max_wal_size=${WALMAX}MB" >> "${TSTUNE_FILE}"
-echo "min_wal_size=${WALMIN}MB" >> "${TSTUNE_FILE}"
+WAL_VOLUME_SIZE=$(numfmt --from=auto "${WAL_VOLUME_SIZE}")
 
 # Run tstune
-timescaledb-tune -quiet -conf-path "${TSTUNE_FILE}" -cpus "${CPUS}" -memory "${MEMORY}MB" -yes "$@"
+timescaledb-tune --quiet --conf-path="${TSTUNE_FILE}" --cpus="${CPUS}" --memory="${MEMORY}MB" --wal-disk-size="${WAL_VOLUME_SIZE}" --yes "$@"
